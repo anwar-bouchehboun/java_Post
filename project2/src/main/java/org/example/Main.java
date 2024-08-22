df1dc968b2ca3b07fb1414f304ec8872cd248b67
anouar <anouar.ab95@gmail.com>
2024-08-22 03:12:22 +0100

    fin  post

--- project2/src/main/java/org/example/Main.java
+++ project2/src/main/java/org/example/Main.java
@@ -1,13 +1,12 @@
 package org.example;
 
-import org.example.db.DB;
 import org.example.dto.PostDto;
 import org.example.dto.RespDTO.PostDtoResp;
 import org.example.services.impl.PostServicesImpl;
 
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
+
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -17,15 +16,15 @@ public class Main {
     PostServicesImpl imp = new PostServicesImpl();
 
       PostDto create = new PostDto();
-      create.setTitle("bfgggggggggg");
+      create.setTitle("tezrtyui");
       create.setDesc("azertyuiop sdfghjk sdfghj");
       imp.CreatePost(create);
     //update
-    /*    PostDtoResp updatedPost = new PostDtoResp();
+       PostDtoResp updatedPost = new PostDtoResp();
         updatedPost.setId(2);
         updatedPost.setTitle("Updated Post Title");
         updatedPost.setDesc("This is the updated description for the post.");
-   imp.UpdatePost(updatedPost);*/
+   imp.UpdatePost(updatedPost);
 
 
 
